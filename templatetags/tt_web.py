cc3ed813144f3b9133ec7bad58c98e87bac44c93
Blaine F Garrett <blaine@blainegarrett.com>
2013-09-07 16:58:54 -0500

    Removing blainegarrett.com specific template_tag

--- templatetags/tt_web.py
+++ templatetags/tt_web.py
@@ -6,35 +6,6 @@ from django.utils.html import conditional_escape
 
 register = Library()
 
-@register.simple_tag
-def carousel():
-    data = [{
-                'title': u'Se\xf1or Wong Mural',
-                'image_path' : '/static/mural2_.jpg',
-                'description' : 'DUDE!!!!',
-                'link' :  'http://google.com',
-            }
-            ,{
-                'title': 'All Over the Walls',
-                'image_path' : '/static/aotw_.jpg',
-                'description' : 'sdddddddddd',
-                'link' :  'http://google.com',
-            },
-            {
-                'title': 'Live Painting',
-                'image_path' : '/static/livingpainting_.jpg',
-                'description' : 'sfsdfsdfdsfdsfsdfsdfdsfds',
-                'link' :  'http://google.com',
-            }]
-            
-    context = {'panels' : data}
-    
-    rendered_content = render_to_string("homepage_carousel.html", context)
-    
-    return rendered_content
-    
-
-
 @register.filter
 def truncate(val, arg):
     if len(val) > arg - 3:
