1d93158594fd1f09655b73a51ce0ffc1fa38da5f
Blaine F Garrett <blaine@blainegarrett.com>
2013-09-14 12:36:36 -0500

    Updating default template structure for plugins. Adding plugins/<plugin_name>/templates/ as a source.

--- core/template/loaders.py
+++ core/template/loaders.py
@@ -1,22 +1,40 @@
+"""
+Merkabah Template Loaders
+"""
 from django.template import TemplateDoesNotExist
-import logging
 import settings
 
+
 def load_template_source(template_name, template_dirs=None):
-    '''
+    """
     Loader for Merkabah templates
-    '''
-    
+    """
+
     if (template_name.find('merkabah/') == 0):
         template_name = '%stemplates/%s' % (settings.MERKABAH_PATH, template_name[9:])
-        
-        logging.warning(template_name)
-        
+
         try:
             return open(template_name).read(), template_name
         except IOError:
-            logging.error(template_name)
             pass
-    raise TemplateDoesNotExist, template_name
-    
+
+    if (template_name.find('plugins/') == 0):
+        # This is loading the default plugin templates
+
+        chunks = template_name.split('/')
+        plugin_module = chunks[1]
+
+        # Check if this is an installed plugin
+        if plugin_module in settings.INSTALLED_PLUGINS:
+            pt_path = '%s/%s/templates' % (settings.PLUGIN_PATH, plugin_module)
+            t_path = '/'.join(chunks[2:])
+            template_name = '%s/%s' % (pt_path, t_path)
+
+            try:
+                return open(template_name).read(), template_name
+            except IOError:
+                pass
+
+    raise TemplateDoesNotExist(template_name)
+
 load_template_source.is_usable = True
--- settings.py
+++ settings.py
@@ -3,11 +3,16 @@ Merkabah Base Settings
 """
 
 import os.path
+
 ROOT_URLCONF = 'urls'
+
 DEBUG=True
+
 APPEND_SLASH = True
+
 PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
-TEMPLATE_DIRS = (os.path.join(PROJECT_DIR, "merkabah/templates"),)
+
+TEMPLATE_DIRS = ( os.path.join(PROJECT_DIR, 'merkabah/templates'), )
 
 INSTALLED_APPS = ('merkabah')
 
