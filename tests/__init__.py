7fed150cfd000eaf6ed069527ff6ad4d64554455
blaine <blaine@blainegarrett.com>
2013-12-29 15:02:11 -0600

    Adding test base class.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,11 @@
 """
 Unit tests for Merkabah
 """
+import unittest 
+
+
+class MerkabahTestCaseBase(unittest.TestCase):
+    pass
+    
+class MerkabahPluginTestCaseBase(MerkabahTestCaseBase):
+    pass
